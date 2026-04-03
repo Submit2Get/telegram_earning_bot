a2beea68cdb9506a3dd936229a45ab5f91b45b61
@Submit2Get <submit2get@gmail.com>
2026-04-03 20:41:21 +0530

    fix telegram version

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
